072c9c987ae17474975e9d7cfd2694a9c7b6247f
victor30518 <victor30518@gmail.com>
2018-05-11 00:38:45 +0800

    Update hw2_seq2seq.sh

--- hw2/hw2_2/hw2_seq2seq.sh
+++ hw2/hw2_2/hw2_seq2seq.sh
@@ -1,2 +1,3 @@
-wget 'https://www.dropbox.com/s/kttbyi66zblee14/120000_64.tar?dl=1' -O 120000_64.tar
+#!/bin/bash
+wget -O 120000_64.tar https://www.dropbox.com/s/kttbyi66zblee14/120000_64.tar?dl=1
 python3 hw2_seq2seq.py --EVAL --input_file=$1 --output_file=$2
