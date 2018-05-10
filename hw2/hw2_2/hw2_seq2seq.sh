84521979b081b1aad19648bb972f6aa4f0f9eb1a
ChuChenLi <32381406+ChuChenLi@users.noreply.github.com>
2018-05-11 02:11:47 +0800

    Update hw2_seq2seq.sh

--- hw2/hw2_2/hw2_seq2seq.sh
+++ hw2/hw2_2/hw2_seq2seq.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -O 120000_64.tar https://www.dropbox.com/s/kttbyi66zblee14/120000_64.tar?dl=1
-python3 hw2_seq2seq.py --EVAL --input_file=$1 --output_file=$2
+python3 hw2_seq2seq.py --EVAL --input_file=$1 --output_file=$2 --loadFilename=120000_64.tar
