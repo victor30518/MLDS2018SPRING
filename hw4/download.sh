5a53759d5bac69f01d3cca60f3b1fb1676a60479
ChuChenLi <32381406+ChuChenLi@users.noreply.github.com>
2018-07-05 20:22:46 +0800

    Update download.sh

--- hw4/download.sh
+++ hw4/download.sh
@@ -1,3 +1,3 @@
 
-wget 'https://www.dropbox.com/s/ol642o0m5v9hk4b/tf_DQN-14720000.data-00000-of-00001?dl=1' -O tf_DQN-14720000.data-00000-of-00001
+wget 'https://www.dropbox.com/s/qqlm8iq5bkarml7/Best.data-00000-of-00001?dl=1' -O Best.data-00000-of-00001
 
